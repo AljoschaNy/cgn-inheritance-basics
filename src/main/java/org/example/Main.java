4a65e5d701fa0032b65ec978a2d28ab9469249c7
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-27 11:31:13 +0200

    Create instances of Vehicle and Car

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.vehicle.Car;
+import org.example.vehicle.Vehicle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("BMW","330",2018,5);
+        Vehicle car2 = new Vehicle("Toyota","Corolla",2014);
+
+        System.out.println(car1);
+        System.out.println(car2);
     }
 }
